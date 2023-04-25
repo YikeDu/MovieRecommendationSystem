391e5f4159b299831f1935b4ff2fe3e44420944a
yikedu <duyikee@gmail.com>
2023-04-25 15:10:32 +0100

    mapper/CollectMapper.class

--- movie--hd-master/src/main/java/com/example/dxw/movie/mapper/LbMapper.java
+++ movie--hd-master/src/main/java/com/example/dxw/movie/mapper/LbMapper.java
@@ -3,14 +3,7 @@ package com.example.dxw.movie.mapper;
 import com.example.dxw.movie.pojo.LbDO;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
-/**
- * <p>
- *  Mapper 接口
- * </p>
- *
- * @author xianwen.deng
- * @since 2023-02-24
- */
+
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
